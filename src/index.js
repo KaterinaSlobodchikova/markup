ff006c5b2c83a375ea7e4df1ff26e9fdcab061c4
Katerina Slobodchikova <k.drazhina@gmail.com>
2023-02-09 11:25:20 +0300

    rick and morty api

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ window.onload = () => {
   const squareWidth = square.getBoundingClientRect().width;
   timeoutAnimate(maxXPosition, containerPad, squareWidth);
   frameAnimate(maxXPosition, containerPad, squareWidth);
+  showFourCharacters();
 };
 
 function timeoutAnimate(maxXPosition, containerPad, squareWidth) {
@@ -35,7 +36,7 @@ function frameAnimate(maxXPosition, containerPad, squareWidth) {
   let square = document.getElementById("animatejs");
   let prevTime = 0;
   let position = 0;
-  let speed = 50;
+  let speed = 10;
   function startAnimation(time) {
     if (!prevTime) {
       prevTime = time;
@@ -58,56 +59,58 @@ function frameAnimate(maxXPosition, containerPad, squareWidth) {
 //   { value: "value1", text: "text2" },
 // ];
 
-const Options_new = new Array(1000);
+//rickandmortyapi
 
-Options_new.fill({
-  value: 2,
-  text: "Lorem ipsum",
-});
+// перехватить code=200 и вывести в консоль success
 
-Options_new.forEach((option) => {
-  let option_new = document.createElement("option");
-  option_new.text = option.text;
-  option_new.value = option.value;
-  let select = document.getElementById("select");
-  select.appendChild(option_new);
-});
+const axios = require("axios");
+
+axios.interceptors.request.use(
+  function (response) {
+    console.log("Success");
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+// axios({
+//   method: "get",
+//   url: "https://rickandmortyapi.com/api/character",
+// }).then(function (response) {
+//   console.log(response.data.results);
+// });
+
+let characters;
+
+const getCharacters = async () => {
+  try {
+    const response = await axios.get(
+      "https://rickandmortyapi.com/api/character"
+    );
+    characters = response.data.results;
+  } catch (error) {
+    console.log(error);
+  }
+};
+getCharacters();
+
+// const Options_new = new Array(100);
+
+// Options_new.fill({
+//   text: "Lorem ipsum",
+// });
+
+// Options_new.forEach((option) => {
+//   let option_new = document.createElement("option");
+//   option_new.text = option.text;
+//   option_new.value = option.value;
+//   let select = document.getElementById("select");
+//   select.appendChild(option_new);
+// });
 
 // input с выпадающим списком, который можно фильтровать
-
-const Options = new Array(10);
-
-Options.fill(
-  {
-    value: "map",
-  },
-  0,
-  3
-);
-
-Options.fill(
-  {
-    value: "sunrise",
-  },
-  3,
-  5
-);
-
-Options.fill(
-  {
-    value: "ocean",
-  },
-  5,
-  8
-);
-
-Options.fill(
-  {
-    value: "ice-cream",
-  },
-  8,
-  10
-);
 
 // Options.forEach((o) => {
 //   let opt = document.createElement("li");
@@ -120,8 +123,9 @@ Options.fill(
 // });
 
 const dropInput = document.getElementById("input__drop");
+
 dropInput.addEventListener("input", show);
-dropInput.addEventListener("blur", hide);
+// dropInput.addEventListener("blur", hide);
 
 function hide() {
   const li = document.querySelectorAll(".option");
@@ -139,15 +143,86 @@ function show() {
   }
   if (inputText.length == 0) return;
 
-  const filteredOptions = Options.filter((option) =>
-    option.value.toLowerCase().includes(inputText)
-  ).slice(0, 10);
+  const filteredOptions = characters
+    .filter((option) =>
+      option.name.toLowerCase().includes(inputText.toLowerCase())
+    )
+    .slice(0, 10);
+
+  console.log(filteredOptions);
 
   filteredOptions.forEach((item) => {
-    let opt = document.createElement("li");
-    opt.textContent = item.value;
-    opt.classList.add("option");
+    let listElement = document.createElement("li");
+    listElement.textContent = item.name;
+    listElement.dataset.image = item.image;
+    listElement.classList.add("option");
     let ul = document.querySelector(".drop");
-    ul.appendChild(opt);
+    ul.appendChild(listElement);
+  });
+  addEvent();
+}
+
+//выбрать персонажа и отобразить о нем информацию
+
+function addEvent() {
+  const lis = document.querySelectorAll(".option");
+  lis.forEach((link) => {
+    link.addEventListener("click", () => {
+      dropInput.value = link.textContent;
+      removeInfo();
+
+      let paragraph = document.createElement("p");
+      paragraph.textContent = link.textContent;
+      paragraph.classList.add("character__p");
+      let img = document.createElement("img");
+      img.classList.add("character__img");
+      img.src = link.dataset.image;
+      const article = document.getElementById("article__character");
+      article.appendChild(img);
+      article.appendChild(paragraph);
+    });
+  });
+}
+
+function removeInfo() {
+  const article = document.getElementById("article__character");
+  const img = article.querySelector(".character__img");
+  const p = article.querySelector(".character__p");
+  if (img) article.removeChild(img);
+  if (p) article.removeChild(p);
+}
+
+// отобразить 4 героя сразу
+
+let fourCharacters;
+
+const getFourCharacters = async () => {
+  try {
+    const response = await axios.get(
+      "https://rickandmortyapi.com/api/character/125,259, 543, 810"
+    );
+    fourCharacters = response.data;
+    console.log(fourCharacters);
+  } catch (error) {
+    console.log(error);
+  }
+};
+getFourCharacters();
+
+function showFourCharacters() {
+  let article_four = document.querySelectorAll(".article__four");
+  article_four.forEach((item, index) => {
+    let paragraph = document.createElement("p");
+    paragraph.textContent = fourCharacters[index].name;
+    let paragraphSpecies = document.createElement("p");
+    paragraphSpecies.textContent = fourCharacters[index].species;
+    let paragraphStatus = document.createElement("p");
+    paragraphStatus.textContent = fourCharacters[index].status;
+    let img = document.createElement("img");
+    img.src = fourCharacters[index].image;
+    item.append(paragraph);
+    item.append(paragraphSpecies);
+    item.append(paragraphStatus);
+    item.append(img);
   });
 }
